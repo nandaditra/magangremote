c579a1bce532dc53cb21a5fe6f78838d420f98b8
nandaditra <nandaditra56@gmail.com>
2024-06-19 10:04:13 +0700

    first commit

---
+++ app/src/main/java/com/example/magangremote/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.magangremote.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.magangremote.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/example/magangremote/ui/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.magangremote.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import com.example.magangremote.R
+import com.example.magangremote.ui.auth.AuthActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, AuthActivity::class.java )
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}

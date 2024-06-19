bee17aa963340efedae37c663baeadf14072fc01
nandaditra <nandaditra56@gmail.com>
2024-06-19 14:16:40 +0700

    add some ui in login, signup, lupa password

--- app/src/main/java/com/example/magangremote/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/magangremote/ui/auth/AuthActivity.kt
@@ -2,11 +2,25 @@ package com.example.magangremote.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.magangremote.R
+import com.example.magangremote.ui.auth.signup.SignUpFragment
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val fragmentManager = supportFragmentManager
+        val signUpFragment = SignUpFragment()
+        val fragment = fragmentManager.findFragmentByTag(SignUpFragment::class.java.simpleName)
+
+        if (fragment !is SignUpFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + SignUpFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, signUpFragment, SignUpFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
